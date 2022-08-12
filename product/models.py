e51d40213078e419e7918659e126d199f1ce99e6
Daegeun Jeong <wjdeorms27@naver.com>
2022-08-12 16:56:09 +0900

    add project

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User as UserModel
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    register_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.product_name
+    
+class Subscribe(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    buy_date = models.DateField(auto_now_add=True)
+    start_subscribe = models.DateField(auto_now_add=True)
+    end_subscribe = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.user} -> {self.product} : {self.start_subscribe}"
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from .models import Product as ProductModel
+from .serializers import ProductSerializer
+class ProductAPIView(APIView):
+    def get(self, request):
+        product_list = ProductModel.objects.filter(is_active=True)
+        serialized_data = ProductSerializer(product_list).data
+        
+        return Response(serialized_data, status=status.HTTP_200_OK)
